5295f79ad469f3f6d1b37830c9ee51547d1328ce
Ivan Johnson <git@IvanJohnson.net>
2020-02-26 14:42:04 -0600

    Cleanup formatting

--- main.ino/main.ino
+++ main.ino/main.ino
@@ -1,12 +1,14 @@
 #include "HX711.h"
 #include <avr/sleep.h>
+
 // HX711 circuit wiring
 const int LOADCELL_DOUT_PIN = 4;
 const int LOADCELL_SCK_PIN = 5;
 
 HX711 scale;
 
-void setup() {
+void setup()
+{
   Serial.begin(57600);
   pinMode(2,INPUT_PULLUP);
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
@@ -18,9 +20,8 @@ void setup() {
   sleep_cpu();
 }
 
-void loop() {
-  
-  
+void loop()
+{
   if (scale.is_ready()) {
     float x = scale.get_units(10);
     Serial.println(x);
@@ -29,7 +30,6 @@ void loop() {
   }
 
   delay(100);
-  
 }
 
 void wakeUp()
@@ -38,4 +38,3 @@ void wakeUp()
   sleep_disable();
   detachInterrupt(digitalPinToInterrupt(2));
 }
-
