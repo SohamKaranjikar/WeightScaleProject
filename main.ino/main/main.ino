d20d022e6815861803ffea83fb071d54131f6598
Ivan Johnson <itj2@siebl-2327-04.ews.illinois.edu>
2020-03-02 15:54:53 -0600

    Implement autosleep

--- main.ino/main/main.ino
+++ main.ino/main/main.ino
@@ -10,25 +10,35 @@ HX711 scale;
 void setup()
 {
   Serial.begin(57600);
+  Serial.println("Hello, World!");
+//  delay(1000);
+
   pinMode(2,INPUT_PULLUP);
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale.set_scale(-22800);
   scale.tare();
-  attachInterrupt(digitalPinToInterrupt(2), wakeUp, LOW);
-  sleep_enable();
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  sleep_cpu();
 }
+
+volatile int sleepCount = 0;
 
 void loop()
 {
   if (scale.is_ready()) {
     float x = scale.get_units(10);
     Serial.println(x);
+    sleepCount++;
+    sleepCount%=10;
+    if (sleepCount == 0) {
+      Serial.println("Going to sleep");
+      delay(100);
+      attachInterrupt(digitalPinToInterrupt(2), wakeUp, LOW);
+      sleep_enable();
+      set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+      sleep_cpu();
+    }
   } else {
     Serial.println("HX711 not found.");
   }
-
   delay(100);
 }
 
