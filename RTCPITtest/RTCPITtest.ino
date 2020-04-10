6f981b94d59d64739fda69f8ef105e314ef9c5db
Soham Karanjikar <Soham@Sohams-MacBook-Pro.local>
2020-04-09 21:05:00 -0500

    add test to see if wake up interrupt works

---
+++ RTCPITtest/RTCPITtest.ino
@@ -0,0 +1,84 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <avr/cpufunc.h>
+
+void RTC_init(void);
+void SLPCTRL_init(void);
+
+void RTC_init(void)
+{
+    uint8_t temp;
+    
+    /* Initialize 32.768kHz Oscillator: */
+    /* Disable oscillator: */
+    temp = CLKCTRL.XOSC32KCTRLA;
+    temp &= ~CLKCTRL_ENABLE_bm;
+    /* Writing to protected register */
+    ccp_write_io((void*)&CLKCTRL.XOSC32KCTRLA, temp);
+    
+    while(CLKCTRL.MCLKSTATUS & CLKCTRL_XOSC32KS_bm)
+    {
+        ; /* Wait until XOSC32KS becomes 0 */
+    }
+    
+    /* SEL = 0 (Use External Crystal): */
+    temp = CLKCTRL.XOSC32KCTRLA;
+    temp &= ~CLKCTRL_SEL_bm;
+    /* Writing to protected register */
+    ccp_write_io((void*)&CLKCTRL.XOSC32KCTRLA, temp);
+    
+    /* Enable oscillator: */
+    temp = CLKCTRL.XOSC32KCTRLA;
+    temp |= CLKCTRL_ENABLE_bm;
+    /* Writing to protected register */
+    ccp_write_io((void*)&CLKCTRL.XOSC32KCTRLA, temp);
+    
+    /* Initialize RTC: */
+    while (RTC.STATUS > 0)
+    {
+        ; /* Wait for all register to be synchronized */
+    }
+
+    /* 32.768kHz External Crystal Oscillator (XOSC32K) */
+    RTC.CLKSEL = RTC_CLKSEL_TOSC32K_gc;
+
+    /* Run in debug: enabled */
+    RTC.DBGCTRL = RTC_DBGRUN_bm;
+    
+    RTC.PITINTCTRL = RTC_PI_bm; /* Periodic Interrupt: enabled */
+    
+    RTC.PITCTRLA = RTC_PERIOD_CYC32768_gc /* RTC Clock Cycles 32768 */
+                 | RTC_PITEN_bm; /* Enable: enabled */
+}
+
+void SLPCTRL_init(void)
+{
+    SLPCTRL.CTRLA |= SLPCTRL_SMODE_PDOWN_gc;
+    SLPCTRL.CTRLA |= SLPCTRL_SEN_bm;
+}
+
+ISR(RTC_PIT_vect)
+{
+    /* Clear flag by writing '1': */
+    RTC.PITINTFLAGS = RTC_PI_bm;
+    Serial.println("Wake up");
+    
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(57600);
+  Serial.println("Hello, World!");
+  RTC_init();
+  SLPCTRL_init();
+  sei();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Going to sleep");
+  sleep_cpu();
+  delay(2000);
+
+}
