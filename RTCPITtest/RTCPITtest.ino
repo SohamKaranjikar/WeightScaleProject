7e234b8e3fa86ab3d30429d93e93e03ea0cb7d9e
Soham Karanjikar <Soham@Sohams-MacBook-Pro.local>
2020-04-09 21:22:15 -0500

    update sleep test

--- RTCPITtest/RTCPITtest.ino
+++ RTCPITtest/RTCPITtest.ino
@@ -5,53 +5,6 @@
 
 void RTC_init(void);
 void SLPCTRL_init(void);
-
-//void RTC_init(void)
-//{
-//    uint8_t temp;
-//    
-//    /* Initialize 32.768kHz Oscillator: */
-//    /* Disable oscillator: */
-//    temp = CLKCTRL.XOSC32KCTRLA;
-//    temp &= ~CLKCTRL_ENABLE_bm;
-//    /* Writing to protected register */
-//    CLKCTRL.XOSC32KCTRLA |= temp;
-//    
-//    while(CLKCTRL.MCLKSTATUS & CLKCTRL_XOSC32KS_bm)
-//    {
-//        ; /* Wait until XOSC32KS becomes 0 */
-//    }
-//    
-//    /* SEL = 0 (Use External Crystal): */
-//    temp = CLKCTRL.XOSC32KCTRLA;
-//    temp &= ~CLKCTRL_SEL_bm;
-//    /* Writing to protected register */
-//    CLKCTRL.XOSC32KCTRLA |= temp;
-//    
-//    /* Enable oscillator: */
-//    temp = CLKCTRL.XOSC32KCTRLA;
-//    temp |= CLKCTRL_ENABLE_bm;
-//    /* Writing to protected register */
-//    CLKCTRL.XOSC32KCTRLA |= temp;
-//    
-//    /* Initialize RTC: */
-//    while (RTC.STATUS > 0)
-//    {
-//        ; /* Wait for all register to be synchronized */
-//    }
-//
-//    /* 32.768kHz External Crystal Oscillator (XOSC32K) */
-//    RTC.CLKSEL = RTC_CLKSEL_TOSC32K_gc;
-//
-//    /* Run in debug: enabled */
-//    RTC.DBGCTRL = RTC_DBGRUN_bm;
-//    
-//    RTC.PITINTCTRL = RTC_PI_bm; /* Periodic Interrupt: enabled */
-//    
-//    RTC.PITCTRLA = RTC_PERIOD_CYC32768_gc /* RTC Clock Cycles 32768 */
-//                 | RTC_PITEN_bm; /* Enable: enabled */
-//}
-//
 void SLPCTRL_init(void)
 {
     SLPCTRL.CTRLA |= SLPCTRL_SMODE_PDOWN_gc;
