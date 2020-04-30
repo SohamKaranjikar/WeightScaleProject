303c9f294b5c20e8f26e0d06ea526c63a1906fea
Ivan Johnson <git@IvanJohnson.net>
2020-04-30 11:30:06 -0500

    Make code prettier

--- BathroomScale/BathroomScale.ino
+++ BathroomScale/BathroomScale.ino
@@ -7,7 +7,7 @@
 #include <avr/power.h>
 #include <avr/io.h>
 #include <avr/cpufunc.h>
-// HX711 circuit wiring
+
 const int LOADCELL_DOUT_PIN = 4;
 const int LOADCELL_SCK_PIN = 5;
 
@@ -18,49 +18,17 @@ void RTC_init(void)
     cli();
     uint8_t temp;
 
-    /* Initialize 32.768kHz Oscillator: */
-    /* Disable oscillator: */
-    //temp = CLKCTRL.INT1K;
     RTC.CLKSEL |= B00000001;
-    //temp &= ~CLKCTRL_ENABLE_bm;
-    /* Writing to protected register */
-    //CLKCTRL.XOSC32KCTRLA = temp;
-
-//    while(CLKCTRL.MCLKSTATUS & CLKCTRL_XOSC32KS_bm)
-//    {
-//        ; /* Wait until XOSC32KS becomes 0 */
-//    }
-
-    /* SEL = 0 (Use External Crystal): */
-//    temp = CLKCTRL.XOSC32KCTRLA;
-//    temp &= ~CLKCTRL_SEL_bm;
-    /* Writing to protected register */
-    //CLKCTRL.XOSC32KCTRLA = temp;
-
-    /* Enable oscillator: */
-//    temp = CLKCTRL.XOSC32KCTRLA;
-//    temp |= CLKCTRL_ENABLE_bm;
-//    /* Writing to protected register */
-//    CLKCTRL.XOSC32KCTRLA = temp;
     RTC.PITCTRLA |= B01001001;
 
     /* Initialize RTC: */
     while (RTC.STATUS > 0)
     {
-        ; /* Wait for all register to be synchronized */
-        Serial.println("Waiting for RTC.Status to be > 0");
+        Serial.println("Waiting for registers to synchronize");
     }
 
-    /* 32.768kHz External Crystal Oscillator (XOSC32K) */
-    //RTC.CLKSEL = RTC_CLKSEL_TOSC32K_gc;
-
-    /* Run in debug: enabled */
-    //RTC.DBGCTRL = ~RTC_DBGRUN_bm;
-
     RTC.PITINTCTRL = 1; /* Periodic Interrupt: enabled */
-    
-//    RTC.PITCTRLA = RTC_PERIOD_CYC16384_gc /* RTC Clock Cycles 32768 */
-//                 | RTC_PITEN_bm; /* Enable: enabled */
+
     sei();
 }
 
@@ -86,41 +54,24 @@ void sleep(int ncycles)
   scale.power_down();
   int nbr_remaining = ncycles; // defines how many cycles should sleep
 
-  // Set sleep to full power down.  Only external interrupts or
-  // the watchdog timer can wake the CPU!
-  //set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-
-  // Turn off the ADC while asleep.
-  //  power_adc_disable();
-
-  while (nbr_remaining > 0){ // while some cycles left, sleep!
+  while (nbr_remaining > 0){
     delay(10);
-    // Enable sleep and enter sleep mode.
     sleep_cpu();
 
     // CPU is now asleep and program execution completely halts!
-    // Once awake, execution will resume at this point if the
-    // watchdog is configured for resume rather than restart
 
-    // When awake, disable sleep mode
-    //sleep_disable();
-
-    // we have slept one time more
     nbr_remaining = nbr_remaining - 1;
   }
 
-  // put everything on again
-  //  power_all_enable();
   scale.power_up();
 }
 
 
 void setup()
 {
-  /* Enable Global Interrupts */
   Serial.begin(57600);
   Serial.println("Hello, World!");
-  
+
   RTC_init();
   SLPCTRL_init();
 
